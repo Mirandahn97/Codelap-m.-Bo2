b387519b86933373a870149c2c20bac93df6692f
Mirandahn97 <mira0249@elev.techcollege.dk>
2025-08-26 10:45:56 +0200

    Opgave 1

--- opgavefiler/js/easy.js
+++ opgavefiler/js/easy.js
@@ -1,9 +1,17 @@
 /* opgave 1 */
 console.group("opgave 1");
-
+const person = {
+    name: "Miranda",
+    age: 28,
+    job: "Elev"
+};
+console.log(person)
 console.groupEnd();
 
 /* opgave 2 */
+console.log(person.name);
+console.log(person.job);
+
 console.group("opgave 2");
 
 
